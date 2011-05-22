fac835a5277edec59e6d157d51cd0248ae9b26dd
straup <aaron@aaronland.net>
2011-05-22 22:19:55 +0200

    flesh out flickr_push_subscribe

--- www/include/lib_flickr_push.php
+++ www/include/lib_flickr_push.php
@@ -1,23 +1,26 @@
 <?php
 
-	# write me
+	loadlib("flickr");
 
 	#################################################################
 
 	function flickr_push_subscribe($flickr_user, $subscription){
 
-		$callback = "{$GLOBALS['cfg']['abs_root_url']}push/{$secret_url}/";
+		$callback = "{$GLOBALS['cfg']['abs_root_url']}push/{$subscription['secret_url']}/";
 
 		$method = 'flickr.push.subscribe';
 
 		$args = array(
-			'auth_token' => 'fix me',
+			'auth_token' => $flickr_user['auth_token'],
 			'topic' => $subscription['type'],
 			'mode' => 'subscribe',
 			'verify' => 'sync',
-			'verify_token' => 'fix me',
+			'verify_token' => $subscription['verify_token'],
 			'callback' => $callback,
 		);
+
+		$rsp = flickr_api_call($method, $args);
+		return $rsp;
 	}
 
 	#################################################################
