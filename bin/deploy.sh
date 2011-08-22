b8a971f7d6dcd5847f71ad41c070273eb65ae2b0
straup <aaron@aaronland.net>
2011-08-22 18:10:37 +0200

    do not run deploy in dry-runmode

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-rsync -arvun --exclude '.git' --exclude 'templates_c' --exclude '*~' /home/asc/pua.spum.org/dev/ /home/asc/pua.spum.org/www/
+rsync -arvu --exclude '.git' --exclude 'templates_c' --exclude '*~' /home/asc/pua.spum.org/dev/ /home/asc/pua.spum.org/www/
